ea48f2f9db58cc07d96365a0fc2ff12c7f5630c3
Juan Malagon <48437681+juanmalagon@users.noreply.github.com>
2024-07-23 15:07:56 +0200

    FIX: linting

--- utils/query_tools.py
+++ utils/query_tools.py
@@ -3,8 +3,8 @@ import re
 import pandas as pd
 
 from utils import country_lists as cl
-from utils import examples as ex
 
+# from utils import examples as ex
 # from utils.functions import scopus_query_list_constructor
 
 # Scopus
@@ -25,19 +25,23 @@ def language_bias_tool(query: str) -> str:
 
 def publication_bias_tool(query: str) -> str:
     """
-    Returns a query without the language restriction.
+    Returns a query including the grey literature.
     """
     return re.sub(r"(AND\s)*SRCTYPE\(\w+\)", "", query)
 
 
 def find_localization_in_text(
-    text: str, countries: list[str] = cl.countries, demonyms: list[str] = cl.demonyms
+    text: str,
+    countries: list[str] = cl.countries,
+    demonyms: list[str] = cl.demonyms
 ) -> bool:
     """
     Returns True if any country name or demonym is found in the text.
     """
     text_words = text.lower().split()
-    if any(location.lower() in text_words for location in countries + demonyms):
+    if any(
+        location.lower() in text_words for location in countries + demonyms
+    ):
         return True
     else:
         return False
@@ -50,51 +54,11 @@ def determine_localization_in_title(
     Add column 'localization_in_title' (boolean)
     """
     df_copy = df.copy()
-    df_copy["localization_in_title"] = df["dc:title"].apply(find_localization_in_text)
+    df_copy["localization_in_title"] = df["dc:title"].apply(
+        find_localization_in_text
+    )
     return df_copy
 
-
-# # Original query by the authors
-# scopus_original_query = ex.mergoni_2021_scopus_query
-
-# # Language-bias-tool: initial query after removing the language restriction
-# scopus_lang_bias_tool_query = language_bias_tool(scopus_original_query)
-
-# # Localization-bias-tool: queries including country names and demonyms
-# # Queries for Western, Educated, Industrialized, Rich and Democratic (WEIRD)
-# # countries
-# scopus_local_bias_tool_queries__weird_countries = scopus_query_list_constructor(
-#     scopus_original_query, cl.weird_countries, step=20, search_field="TITLE-ABS-KEY"
-# )
-# scopus_local_bias_tool_queries__weird_demonyms = scopus_query_list_constructor(
-#     scopus_original_query, cl.weird_demonyms, step=20, search_field="TITLE-ABS-KEY"
-# )
-# scopus_local_bias_tool_queries_weird = (
-#     scopus_local_bias_tool_queries__weird_countries
-#     + scopus_local_bias_tool_queries__weird_demonyms
-# )
-# # Queries for non-WEIRD countries
-# scopus_local_bias_tool_queries__non_weird_countries = scopus_query_list_constructor(
-#     scopus_original_query, cl.non_weird_countries, step=20, search_field="TITLE-ABS-KEY"
-# )
-# scopus_local_bias_tool_queries__non_weird_demonyms = scopus_query_list_constructor(
-#     scopus_original_query, cl.non_weird_demonyms, step=20, search_field="TITLE-ABS-KEY"
-# )
-# scopus_local_bias_tool_queries_non_weird = (
-#     scopus_local_bias_tool_queries__non_weird_countries
-#     + scopus_local_bias_tool_queries__non_weird_demonyms
-# )
-# # Queries for Latin American and Caribbean (LAC) countries
-# scopus_local_bias_tool_queries__lac_countries = scopus_query_list_constructor(
-#     scopus_original_query, cl.lac_countries, step=20, search_field="TITLE-ABS-KEY"
-# )
-# scopus_local_bias_tool_queries__lac_demonyms = scopus_query_list_constructor(
-#     scopus_original_query, cl.lac_demonyms, step=20, search_field="TITLE-ABS-KEY"
-# )
-# scopus_local_bias_tool_queries_lac = (
-#     scopus_local_bias_tool_queries__lac_countries
-#     + scopus_local_bias_tool_queries__lac_demonyms
-# )
 
 # Publication-bias-tool: queries including the grey literature
 scopus_pub_bias_tool_query = (
